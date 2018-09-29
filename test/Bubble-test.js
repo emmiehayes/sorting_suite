e2accd60e40504137c6ae8d7eddd69e2c6142ac1
Emmie Hayes <emmiehayes2@gmail.com>
2018-09-29 16:52:36 -0600

    create Bubble class, test it exist and that it can have a numberSet

--- test/Bubble-test.js
+++ test/Bubble-test.js
@@ -1,7 +1,19 @@
 const { assert } = require('chai');
+const Bubble = require('../lib/Bubble.js')
 
 describe('Bubble', function () {
   it('should return true', function () {
     assert.equal(true, true);
   });
+
+  it('should have a default set of numbers', function () {
+    var bubble = new Bubble();
+    assert.deepEqual(bubble.numberSet, [4, 5, 8, 3, 2]);
+  });
+
+  it('should have take a set of numbers as arguments', function () {
+    var bubble = new Bubble([7, 3, 5]);
+    assert.deepEqual(bubble.numberSet, [7, 3, 5]);
+  });
+
 });
