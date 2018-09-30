81e62ae5e44e286e301290cf55b6eccf2a3bba96
Emmie Hayes <emmiehayes2@gmail.com>
2018-09-29 18:47:33 -0600

    sorts two numbers

--- test/Bubble-test.js
+++ test/Bubble-test.js
@@ -2,6 +2,7 @@ const { assert } = require('chai');
 const Bubble = require('../lib/Bubble.js')
 
 describe('Bubble', function () {
+  
   it('should return true', function () {
     assert.equal(true, true);
   });
@@ -11,9 +12,20 @@ describe('Bubble', function () {
     assert.deepEqual(bubble.numberSet, [4, 5, 8, 3, 2]);
   });
 
+  it('should have a default state of false for sorted', function () {
+    var bubble = new Bubble();
+    assert.deepEqual(bubble.sorted, false);
+  });
+
   it('should have take a set of numbers as arguments', function () {
     var bubble = new Bubble([7, 3, 5]);
     assert.deepEqual(bubble.numberSet, [7, 3, 5]);
   });
 
+
+  it('should sort two numbers from smallest to largest', function () {
+    var bubble = new Bubble([7, 5]);
+    assert.deepEqual(bubble.numberSet, [7, 5]);
+    assert.deepEqual(bubble.sort(), [5, 7]);
+  });
 });
