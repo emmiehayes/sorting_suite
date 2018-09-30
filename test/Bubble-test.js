b31397acba3d959cfa2f2b2c6234235a55563026
Emmie Hayes <emmiehayes2@gmail.com>
2018-09-29 18:56:13 -0600

    refactor variable declarations in sort, remove unnecessary constructor variable, remove test for unnecessary

--- test/Bubble-test.js
+++ test/Bubble-test.js
@@ -12,21 +12,21 @@ describe('Bubble', function () {
     assert.deepEqual(bubble.numberSet, [4, 5, 8, 3, 2]);
   });
 
-  it('should have a default state of false for sorted', function () {
-    var bubble = new Bubble();
-    assert.deepEqual(bubble.sorted, false);
-  });
-
   it('should have take a set of numbers as arguments', function () {
     var bubble = new Bubble([7, 3, 5]);
     assert.deepEqual(bubble.numberSet, [7, 3, 5]);
   });
 
-
   it('should sort two numbers from smallest to largest', function () {
     var bubble = new Bubble([7, 5]);
     assert.deepEqual(bubble.numberSet, [7, 5]);
     assert.deepEqual(bubble.sort(), [5, 7]);
+  });
+
+  it('should sort three numbers from smallest to largest', function () {
+    var bubble = new Bubble([7, 5, 9]);
+    assert.deepEqual(bubble.numberSet, [7, 5, 9]);
+    assert.deepEqual(bubble.sort(), [5, 7, 9]);
   });
 
   it('should sort three numbers from smallest to largest', function () {
